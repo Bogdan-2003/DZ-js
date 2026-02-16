d60470b91eebea2c3130797a20b23610a1fb57bd
BogdanKnysh <knysbogdan74@gmail.com>
2026-02-16 15:57:56 +0300

    Шифровка 2

--- 8-crypto/app.js
+++ 8-crypto/app.js
@@ -1,24 +1,27 @@
 function crypto(password) {
-    const n = password.length;
-  const half = Math.floor(n / 2);
+  const n = password.length;
+  const half = n / 2; 
   
+  const left = password.substring(0, half);
+  const right = password.substring(half);
   
-  const leftPart = password.slice(0, half);
-  const rightPart = password.slice(half);
+  const reversedLeft = left.split('').reverse().join('');
   
-  
-  const reversedLeft = leftPart.split('').reverse().join('');
-  
-  
-  let swappedRight;
-  if (rightPart.length > 1) {
-    swappedRight = 
-      rightPart[rightPart.length - 1] +  
-      rightPart.slice(1, -1) +           
-      rightPart[0];                      
+  let processedRight;
+  if (right.length > 1) {
+    const firstChar = right[0];
+    const lastChar = right[right.length - 1];
+    const middle = right.substring(1, right.length - 1);
+    processedRight = lastChar + middle + firstChar;
   } else {
-    swappedRight = rightPart;
+    processedRight = right;
   }
   
-  return reversedLeft + swappedRight;
+  return reversedLeft + processedRight;
+}
+
+function check(encrypted, candidate) {
+  const decrypted = crypto(encrypted);
+  
+  return decrypted === candidate;
 }
